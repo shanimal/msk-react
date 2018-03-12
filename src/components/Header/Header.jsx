6754bbc8b922fbbe3d6bec41b7b79eeccb4ffc22
Shannon Thrasher <sthrasher@mz.com>
2018-03-12 02:03:54 -0700

    responsive except home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,12 +9,14 @@ class Header extends Component {
   render() {
     const currentTab = this.props.location.pathname.slice(1) || 'home';
     const { onMenuChange } = this.props;
-    return <header className="Header">
-      <div className="topper" />
-      <Menu currentTab={currentTab} onChange={onMenuChange} />
-      <Social />
-      <img src={logo} className="App-logo" alt="logo" />
-    </header>
+    return (
+      <header className="Header">
+        <img src={logo} className="logo" alt="logo" />
+        <div className="topper" />
+        <Menu currentTab={currentTab} onChange={onMenuChange} />
+        <Social />
+      </header>
+    );
   }
 }
 
--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -8,7 +8,9 @@ class Social extends Component {
       <div className="Social">
         <iframe title="Facebook" className="fblike" scrolling="no" frameBorder="0" allowtransparency="true" src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fhome.php%23%21%2Fpages%2FMain-Street-Kickboxing-Hollister-Ca%2F121595357867256&amp;layout=button_count&amp;show_faces=false&amp;width=450&amp;action=like&amp;font=trebuchet+ms&amp;colorscheme=light&amp;height=21"></iframe>
         <div className="fbnews siteFont">
-          <a ng-href="fbnews" className="primary" href="fbnews">facebook Live News Feed</a>
+          <a className="primary" href="fbnews">
+            FB News <span className="desktop-only">- Live Feed</span>
+          </a>
         </div>
         <div className="action_arrow" />
         <div className="action">
@@ -22,16 +24,18 @@ class Social extends Component {
           </a>
           <Link to="/shop" className="shopmsk linkWhite siteFont" href="shop">Shop MSK</Link>
         </div>
-        <a
-          className="fblink"
-          href="http://www.facebook.com/pages/Main-Street-Kickboxing-Hollister-Ca/121595357867256"
-          target="_blank" rel="noopener noreferrer"
-        > </a>
-        <a
-          className="ytlink"
-          href="http://www.youtube.com/user/mainstreetkickboxing"
-          target="_blank" rel="noopener noreferrer"
-        > </a>
+        <div className="links">
+          <a
+            className="fblink"
+            href="http://www.facebook.com/pages/Main-Street-Kickboxing-Hollister-Ca/121595357867256"
+            target="_blank" rel="noopener noreferrer"
+          > </a>
+          <a
+            className="ytlink"
+            href="http://www.youtube.com/user/mainstreetkickboxing"
+            target="_blank" rel="noopener noreferrer"
+          > </a>
+        </div>
       </div>
     );
   }
