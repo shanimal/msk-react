25a2a334be258a843302d983f484ba1a1d6a51ab
Shannon Thrasher <sthrasher@mz.com>
2018-03-14 00:23:38 -0700

    fbnews width

--- src/components/FBNews/FBNews.jsx
+++ src/components/FBNews/FBNews.jsx
@@ -15,7 +15,7 @@ class Page extends Component {
               frameBorder="0"
               id="fbnews"
               scrolling="no"
-              src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FMSK-Main-Street-Kickboxing-Hollister-Ca%2F121595357867256&amp;colorscheme=dark&amp;show_faces=false&amp;stream=true&amp;header=false&amp;height=900&amp;width=900"
+              src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FMSK-Main-Street-Kickboxing-Hollister-Ca%2F121595357867256&amp;colorscheme=dark&amp;show_faces=false&amp;stream=true&amp;header=false&amp;height=900"
               title="fbnews"
               target="_top"
             />
